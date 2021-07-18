8a03dca4be3cc42a96d9a9f4a6739c9776a13b98
elSyarif <id.syarif@gmail.com>
2021-07-18 20:21:38 +0700

    module permission add endpoint routes

--- src/modules/permissions/permissionController.js
+++ src/modules/permissions/permissionController.js
@@ -23,3 +23,48 @@ export const getPermission = asyncHandler(async(req, res, next) =>{
         data : permission
     })
 })
+
+export const updatePermission = asyncHandler(async(req, res, next) =>{
+    try {
+        const id = req.params.id
+        const {name, description} = req.body
+        const permission = await Permission.findById(id)
+
+        if(permission){
+            permission.name = name || permission.name
+            permission.description = description || permission.description
+
+            const update = await permission.save()
+
+            res.json({
+                message : "Update permission successfully",
+                data: update
+            })
+        }else{
+            throw new Error('Permission not found');
+        }
+    } catch (error) {
+        throw new Error(error.message);
+    }
+})
+
+export const deletePermission = asyncHandler(async(req, res, next) =>{
+    try {
+        const id = req.params.id
+        
+        const permission = await Permission.findById(id)
+
+        if(permission){
+            const del = await Permission.deleteOne(id)
+
+            res.json({
+                message : "Delete permission successfully",
+                data: del
+            })
+        }else{
+            throw new Error('Permission not found');
+        }
+    } catch (error) {
+        throw new Error(error.message);
+    }
+})
--- src/routes/permissionRoutes.js
+++ src/routes/permissionRoutes.js
@@ -1,9 +1,12 @@
 import express from 'express'
-import { createPermission, getPermission } from '../modules/permissions/permissionController.js'
+import { createPermission, deletePermission, getPermission, updatePermission } from '../modules/permissions/permissionController.js'
+import { admin, permit, protect } from '../middleware/authMiddleware.js';
 
 const permissionRoutes = express.Router()
 
-permissionRoutes.get('/permission', getPermission)
-permissionRoutes.post('/permission', createPermission)
+permissionRoutes.get('/permission', protect, getPermission)
+permissionRoutes.post('/permission', protect, admin, createPermission)
+permissionRoutes.put('/permission/:id',protect,admin, updatePermission)
+permissionRoutes.delete('/permission/:id', protect, admin, deletePermission)
 
 export default permissionRoutes
